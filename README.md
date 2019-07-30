2293c255c37e444b692ab2fbfbfbc9df65afb5fa
naohiro2g <naohiro2g@code2create.club>
2019-07-30 21:47:19 +0900

    update

--- README.md
+++ README.md
@@ -2,28 +2,28 @@ https://dordnung.de/raspberrypi-ledstrip/ws2812
 http://www.neko.ne.jp/~freewing/raspberry_pi/raspberry_pi_gpio_pwm_ws2812b_rgb_led/
 
 
+## Test drive
 
+You need to be the super user. Set -c to clear the display on exit.
+LED_PIN=18
+
+```
+sudo python3 examples/test.py -c --brightness 20 --pixel_count 16
+sudo python3 examples/strandtest.py -c
+
+```
 
 ## Wiring
-
 
 WS2812  |  RPi           |  Ext Power 
 |-------|----------------|--------------|
 GND     |  GND           |
-DIN     |  GPIO18 or 12  |
-VDC     |  (3.3V or 5V)  | 3.3V or 5V
+DIN     |  GPIO18 (or 12)|
+VDC     |  3.3V (or 5V)  | 3.3V (or 5V)
 GND     |                | GND
 
 The module says "4-7VDC" but it works at both 3.3V or 5V of RasPi.
 
-## Test drive
-
-You need to be the super user. Set -c to clear the display on exit. 
-LED_PIN=18, LED_COUNT=16 for 2 units, brightness is 64/255 which is bright enough.
-
-```
-sudo python3 examples/strandtest.py -c
-```
 
 # RPi WS281x Python
 
--- examples/test.py
+++ examples/test.py
@@ -9,18 +9,19 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
 parser.add_argument('-b', '--brightness', type=int, default=20, help='brightness 0 to 255')
+parser.add_argument('-p', '--pixel_count', type=int, default=8, help='how many pixels?')
 args = parser.parse_args()
 
 
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (12/18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = args.brightness     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_COUNT      = args.pixel_count  # Number of LED pixels.
+LED_PIN        = 18                # GPIO pin connected to the pixels (12/18:pwm0, 13/19:pwm1).
+#LED_PIN        = 10               # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000            # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10                # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = args.brightness   # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False             # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0                 # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 # Create NeoPixel object with appropriate configuration.
