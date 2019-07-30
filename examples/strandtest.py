fd79d1fa0b8294ef187577a44d9167bbe5873bb4
naohiro2g <naohiro2g@code2create.club>
2019-07-30 21:13:09 +0900

    update

--- examples/strandtest.py
+++ examples/strandtest.py
@@ -13,21 +13,12 @@ from rpi_ws281x import *
 import argparse
 
 # LED strip configuration:
-<<<<<<< HEAD
 LED_COUNT      = 8      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (12/18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 16     # Set to 0 for darkest and 255 for brightest
-=======
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 or 12, but they use PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 64     # Set to 0 for darkest and 255 for brightest
->>>>>>> d9d6717e82a454a591f3d3d5643bf6e13a47f3f9
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
